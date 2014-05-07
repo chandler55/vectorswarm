b3f1cb932cae6f77f9458fad1d68cbca13976a36
chandler55 <michaelle@gmail.com>
2014-05-07 02:05:52 -0400

    - more enemy ships

---
+++ unityProject/Assets/Scripts/Game/Enemies/HourGlassEnemy.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HourGlassEnemy : Enemy
+{
+    public float movementDuration = 2.0f;
+
+    private bool mMovingRight = false;
+
+    void Start()
+    {
+        mMovingRight = Position.x > 0;
+        Move();
+    }
+
+    void Update()
+    {
+    }
+
+    void OnDestroy()
+    {
+        Go.killAllTweensWithTarget( transform );
+    }
+
+    void OnCompleteTween( AbstractGoTween tween )
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        mMovingRight = !mMovingRight;
+
+        Vector3 newPos;
+        if ( mMovingRight )
+        {
+            newPos = new Vector3( GameSettings.WORLD_BOUNDARY.x + GameSettings.WORLD_BOUNDARY.width, Position.y, gameObject.transform.position.z );
+        }
+        else
+        {
+            newPos = new Vector3( GameSettings.WORLD_BOUNDARY.x, Position.y, gameObject.transform.position.z );
+        }
+
+        Go.to( transform, movementDuration, new GoTweenConfig().position( newPos ) ).setOnCompleteHandler( OnCompleteTween );
+    }
+
+    public override void DestroyEnemy()
+    {
+        base.DestroyEnemy();
+        Die();
+    }
+
+    public void Die()
+    {
+        Messenger.Broadcast<int>( Events.GameEvents.IncrementScore, 10 );
+        ParticleSystemManager.Instance.CreateEnemyExplosion( Position );
+        ObjectPool.Recycle( this );
+    }
+}
--- unityProject/Assets/Scripts/Game/Enemies/VerticalEnemy.cs
+++ unityProject/Assets/Scripts/Game/Enemies/VerticalEnemy.cs
@@ -5,6 +5,7 @@ public class VerticalEnemy : Enemy
 {
     public Transform positionTarget;
     public float movementDuration = 2.0f;
+    public float rotationDuration = 0.5f;
 
     private bool mMovingTowardsPosition2 = false;
     private Vector3 mPosition1 = Vector3.zero;
@@ -17,10 +18,10 @@ public class VerticalEnemy : Enemy
         {
             mPosition2 = positionTarget.position;
         }
-        
+
         mMovingTowardsPosition2 = true;
 
-        Move();
+        Rotate();
     }
 
     void Update()
@@ -32,14 +33,35 @@ public class VerticalEnemy : Enemy
         Go.killAllTweensWithTarget( transform );
     }
 
-    void OnCompleteTween( AbstractGoTween tween )
+    void OnCompleteMove( AbstractGoTween tween )
+    {
+        mMovingTowardsPosition2 = !mMovingTowardsPosition2;
+
+        Rotate();
+    }
+
+    void OnCompleteRotate( AbstractGoTween tween )
     {
         Move();
     }
 
+    void Rotate()
+    {
+        Quaternion newRot = Quaternion.identity;
+        if ( mMovingTowardsPosition2 )
+        {
+            newRot = Quaternion.Euler( new Vector3( 0, 0, -180 ) );
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Euler( new Vector3( 0, 0, 180 ) );
+        }
+
+        Go.to( transform, rotationDuration, new GoTweenConfig().localRotation( newRot ) ).setOnCompleteHandler( OnCompleteRotate );
+    }
+
     void Move()
     {
-        mMovingTowardsPosition2 = !mMovingTowardsPosition2;
 
         Vector3 newPos;
         if ( mMovingTowardsPosition2 )
@@ -51,7 +73,7 @@ public class VerticalEnemy : Enemy
             newPos = new Vector3( mPosition1.x, mPosition1.y, gameObject.transform.position.z );
         }
 
-        Go.to( transform, movementDuration, new GoTweenConfig().position( newPos ) ).setOnCompleteHandler( OnCompleteTween );
+        Go.to( transform, movementDuration, new GoTweenConfig().position( newPos ) ).setOnCompleteHandler( OnCompleteMove );
     }
 
     public override void DestroyEnemy()
